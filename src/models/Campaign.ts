c4b9fbdf8c886f8946d6fe14fb8f44aebc274288
Guilherme Costa <Guilhermeryukaii@gmail.com>
2024-11-22 23:09:53 -0300

    ADD:  adicionando novos campos ao Schema

--- src/models/Campaign.ts
+++ src/models/Campaign.ts
@@ -1,10 +1,14 @@
-// src/models/Campaign.ts
 import mongoose from "mongoose";
 
 const campaignSchema = new mongoose.Schema({
   name: { type: String, required: true },
   integrationName: { type: String, required: true },
+  tipoEvento: { type: String, required: true },
   messageTemplate: { type: String, required: true },
+  delay: {
+    time: { type: String, required: true },
+    counter: { type: String, required: true },
+  },
   createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   createdAt: { type: Date, default: Date.now },
   status: { type: String, default: "pending" },
