092254c99ab379b1a3aab50df4502e4c398fdb00
Guilherme Costa <Guilhermeryukaii@gmail.com>
2024-12-12 13:59:10 -0300

    Revert "no message"
    This reverts commit 179237e2f331b27e84a4f7980360fbfb62293c09.

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,4 +1,6 @@
 import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import redisClient from "../utils/redisClient";
 
 const authMiddleware = async (
   req: Request,
@@ -12,10 +14,14 @@ const authMiddleware = async (
       res.status(401).send("Token não fornecido");
       return;
     }
-    const secret = process.env.SECRET_KEY;
-    console.log(secret);
-    console.log(authHeader);
-    if (authHeader === secret) {
+
+    const token = authHeader.split(" ")[1];
+    const decoded: any = jwt.verify(token, process.env.JWT_SECRET as string);
+
+    // Verificar o token no Redis
+    const cachedToken = await redisClient.get(decoded.userId);
+    if (cachedToken && cachedToken === token) {
+      req.user = decoded; // Adiciona o usuário à requisição para uso futuro
       next();
     } else {
       res.status(401).send("Token inválido ou expirado");
